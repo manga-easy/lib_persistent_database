f62a68e46f6775b5346c474b79c0248ee4e79aaf
lucas <lucas.cardo123@hotmail.com>
2023-06-03 22:23:18 -0400

    remove hive

--- lib/manga_easy_persistent_database_service.dart
+++ lib/manga_easy_persistent_database_service.dart
@@ -4,8 +4,6 @@
 library;
 
 export 'src/persistent_database_service.dart';
-export 'src/hive/hive_db_service.dart';
-export 'src/hive/box_hive_enum.dart';
 export 'src/sembast/sembast_db_service.dart';
 export 'src/sembast/store_sembast_enum.dart';
 export 'src/user_preference/key_preference.dart';
