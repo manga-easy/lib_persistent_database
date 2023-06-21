7c22dc3c6e26f1ace53190198dee967b1ba88bfd
lucas <lucas.cardo123@hotmail.com>
2023-06-20 21:57:45 -0400

    Update persistent_database.dart

--- lib/persistent_database.dart
+++ lib/persistent_database.dart
@@ -3,8 +3,8 @@
 /// More dartdocs go here.
 library;
 
-export 'src/persistent_database_service.dart';
-export 'src/sembast/sembast_db_service.dart';
+export 'src/persistent_database.dart';
+export 'src/sembast/sembast_database.dart';
 export 'src/sembast/store_sembast_enum.dart';
 export 'src/user_preference/key_preference.dart';
 export 'src/user_preference/preference_service.dart';
